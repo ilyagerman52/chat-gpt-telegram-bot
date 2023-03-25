ee365334ce01e49a4d337076c3ecb08584d7de11
ilyagerman52 <ivgerman@edu.hse.ru>
2023-03-25 07:45:24 +0300

    first version

---
+++ main.py
@@ -0,0 +1,34 @@
+import telebot
+import openai
+import json
+
+from keys import *
+
+bot = telebot.TeleBot(telegram_token)
+openai.api_key = openai_api_key
+
+chats = {}
+
+
+def chat(text, messages):
+    messages.append({"role": "user", "content": text})
+    return openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages
+    )
+
+
+@bot.message_handler(content_types=["text"])
+def handle_text(user_input):
+    chat_id = user_input.chat.id
+    if chat_id not in chats.keys():
+        chats[chat_id] = [
+            {"role": "system", "content": "Hello"},
+        ]
+    response = chat(user_input.text, chats[chat_id])
+    result = response.choices[0]['message']
+    chats[chat_id].append(result)
+    bot.send_message(user_input.chat.id, result['content'])
+
+
+bot.polling()
