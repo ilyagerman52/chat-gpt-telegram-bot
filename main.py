4219b5546002c0e65303391cd9c0de0b85a0bc4a
ilyagerman52 <ivgerman@edu.hse.ru>
2024-01-02 15:06:56 +0000

    working version

--- main.py
+++ main.py
@@ -1,158 +1,80 @@
-import openai
-import telebot
-from telebot import types
-from keys import *
-from sql_db import *
-import time
-import logging
+import os
 import sys
+from datetime import datetime
+import logging
+
+import aiogram.types
+import openai
+from aiogram import Bot
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+
 from utils import HELP_MESSAGE
+from database_connectivity import DataBase
+from utils import get_response
+
+_LOGGER = logging.Logger("default", level=20)
+_handler = logging.StreamHandler(sys.stdout)
+_handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter('[%(asctime)s] [%(name)s] [%(levelname)s]  %(message)s')
+_handler.setFormatter(formatter)
+_LOGGER.addHandler(_handler)
+_LOGGER.info("Initializing bot")
+try:
+    bot = Bot(token=os.environ['BOT_TOKEN'])
+    dp = Dispatcher(bot)
+    _LOGGER.info("bot has been initialized successfully")
+except IndexError as e:
+    _LOGGER.exception(e)
+    raise
+_LOGGER.info("Start initializing history_database")
+database = DataBase("history_database", "database/profiles.db")
+_LOGGER.info("history_database has been initialized")
+
+_LOGGER.info("Setting openai api key")
+try:
+    openai.api_key = os.environ['OPENAI_API_KEY']
+    client = openai.OpenAI()
+    _LOGGER.info("openai api key has been set successfully")
+except IndexError as e:
+    _LOGGER.exception(e)
+    raise
 
 
-# region logger_setup
-_logger = logging.getLogger(__name__)
-_logger.setLevel(logging.INFO)
-stream_handler = logging.StreamHandler(stream=sys.stderr)
-file_handler = logging.FileHandler(filename="info.log")
-formatter = logging.Formatter(fmt='[%(asctime)s: %(levelname)s] %(message)s')
-stream_handler.setFormatter(formatter)
-file_handler.setFormatter(formatter)
-# endregion logger_setup
-
-_logger.info("Start")
-
-_logger.info("Initializing bot")
-bot = telebot.TeleBot(telegram_token)
-_logger.info("Bot has been initialized successfully")
-
-_logger.info("Setting openai api key")
-openai.api_key = openai_api_key
-_logger.info("openai api key has been set successfully")
-
-
-@bot.message_handler(commands=['start'])
-def start_chat(msg):
+@dp.message_handler(commands=['start', 'help'])
+async def start_chat(msg: aiogram.types.Message):
     user_id = msg.chat.id
-    username = msg.from_user.full_name
-    _logger.info(f"Initializing new chat for user {username} with user_id {user_id}")
-    status = UserStatus.USER
-    profile = Profile(user_id=user_id, username=username, parse_mode="Markdown", status=status, key="")
-    if not add_profile(profile):
-        _logger.warning("Warning! Cannot create new user")
-        raise RuntimeError()
+    username = msg.from_user.username
+    new_flg = database.tables['profiles'].get_data(f"uid == {user_id}").fetchall() == []
+    if new_flg:
+        _LOGGER.info(f"Initializing new chat for user {username} with user_id {user_id}")
+        database.tables['profiles'].add_row({'uid': user_id, 'username': username})
+    await msg.reply(HELP_MESSAGE)
 
 
-@bot.message_handler(commands=['help'])
-def show_help(msg):
-    _logger.info(f"Sending `show_help` message: user_id={msg.chat_id}")
-    try:
-        bot.send_message(chat_id=msg.chat_id,
-                         text=HELP_MESSAGE)
-        _logger.info(f"Successfully sent `show_help` message: user_id={msg.chat_id}")
-    except Exception as exc:
-        _logger.exception("Cannot send message in `show_help`.")
-
-
-@bot.message_handler(commands=['profile'])
-def show_profile(msg):
+@dp.message_handler(commands=['clear'])
+async def clear_history(msg: aiogram.types.Message):
     user_id = msg.chat.id
-    profile = get_profile(user_id)
-    username = profile.username
-    pm = profile.parse_mode if profile.parse_mode is not None else "None"
-    status = 'admin' if profile.status == UserStatus.ADMIN else 'user'
-    markup = types.InlineKeyboardMarkup()
-    _logger.info(f"Sending `show_profile` message: user_id={user_id}")
-    try:
-        bot.send_message(chat_id=user_id,
-                         text=f"Profile.\n\nUsername: {username}\nParse mode: {pm}\nStatus: {status}",
-                         reply_markup=markup)
-        _logger.info(f"Sent `show_profile` message: user_id={user_id}")
-    except Exception as exc:
-        _logger.exception(f"Cannot send `show_profile` message: user_id={user_id}.")
+    database.tables['history'].delete_data(condition=f"uid == {user_id}")
 
 
-@bot.message_handler(commands=['clear'])
-def clear_conversation(msg):
+@dp.message_handler(content_types=['text'])
+async def reply_message(msg: aiogram.types.Message):
     user_id = msg.chat.id
-    if not clear_chat_history(user_id):
-        _logger.info(f"History for user {user_id} has been cleared")
-        reply_text = "History has been cleared."
-    else:
-        _logger.warning(f"Cannot clear history for user {user_id}")
-        reply_text = "Cannot clear history."
+    content = msg.text
+    _LOGGER.info(f"Got message from {user_id}")
+    dttm_request = datetime.datetime.now().strftime("%Y-%m-%d %H-%M-%S")
+    database.tables['history'].add_row({'uid': user_id, 'content': content, 'is_bot_response': False, 'dttm': dttm_request})
+    history = database.tables['history'].get_data(f"uid == {user_id}", fields="content, is_bot_response").fetchall()
     try:
-        bot.send_message(chat_id=user_id, text=reply_text)
-    except Exception as exc:
-        _logger.exception(f"Cannot send `clear_conversation` message: user_id={user_id}.")
-
-
-@bot.message_handler(commands=['settings'])
-def settings(msg):
-    user_id = msg.user_id
-    markup = types.InlineKeyboardMarkup()
-    btn_chg_pm = types.InlineKeyboardButton("Change parse mode", callback_data="set_chg_pm")
-    btn_enter_key = types.InlineKeyboardButton("Enter key", callback_data="set_enter_key")
-    markup.add(btn_chg_pm, btn_enter_key)
-    try:
-        bot.send_message(chat_id=user_id, text="Chose parameter that you want to change", reply_markup=markup)
-    except Exception as exc:
-        _logger.exception(f"Cannot send `settings` message: user_id:{user_id}.")
-
-
-@bot.callback_query_handler(lambda call: call.data[:4] == 'set_')
-def edit_profile(call):
-    if call.data == 'set_chg_pm':
-        markup = types.InlineKeyboardMarkup()
-        btn_none = types.InlineKeyboardButton("None", callback_data="pm_none")
-        btn_html = types.InlineKeyboardButton("HTML", callback_data="pm_html")
-        btn_md = types.InlineKeyboardButton("Markdown", callback_data="pm_md")
-        markup.add(btn_none, btn_html, btn_md)
-        try:
-            bot.edit_message_text("Chose new parse mode value:", call.message.chat.id, reply_markup=markup)
-            _logger.info(f"Edited `edit_profile` message: user_id={call.message.chat.id}.")
-        except Exception as exc:
-            _logger.exception(f"Cannot edit `edit_profile` message: user_id={call.message.chat.id}.")
-    elif call.data == 'set_enter_key':
-        _logger.warning("Not implemented")
-        raise NotImplementedError()
-
-
-@bot.callback_query_handler(lambda call: call.data[:3] == 'pm_')
-def set_pm_value(call):
-    new_value = None
-    if call[3:] != "none":
-        new_value = call.data[3:]
-    if update_profile(call.message.chat.id, Field.PARSE_MODE, new_value):
-        _logger.info(f"Updated profile: user_id={call.message.chat.id}")
-    else:
-        _logger.warning(f"Cannot update profile: user_id={call.message.chat.id}")
-
-
-@bot.message_handler(content_types=['text'])
-def reply_message(msg):
-    request_dt = datetime.now()
-    user_id = msg.chat.id
-    profile = get_profile(user_id)
-    if profile.status == UserStatus.BLOCKED:
-        bot.send_message(user_id,
-                         "Sorry, you were banned. If you consider it is a mistake, please contact technical support.")
+        response = get_response(client, history)
+    except openai.RateLimitError as e:
         return
-    request_content = msg.text
-    chat_history = get_chat_history(user_id)
-    if chat_history is None:
-        add_profile(Profile(user_id=user_id,
-                            username=msg.from_user.username,
-                            parse_mode="Markdown",
-                            status=UserStatus.USER,
-                            key=None))
-    chat_history.append({"role": "user", "content": request_content})
-    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=chat_history)
-    response_content = response.choices[0]['message']
-    pm = profile.parse_mode
-    bot.send_message(user_id, response_content, parse_mode=pm)
-    response_dt = datetime.now()
-    add_message_chat(user_id, ChatMessage(role="user", content=request_content, dt=request_dt))
-    add_message_chat(user_id, ChatMessage(role="system", content=response_content, dt=response_dt))
+    await msg.reply(response)
+    dttm_response = datetime.datetime.now().strftime("%Y-%m-%d %H-%M-%S")
+    database.tables['history'].add_row({'uid': user_id, 'content': response, 'is_bot_response': True, 'dttm': dttm_response})
 
 
-bot.polling()
+if __name__ == '__main__':
+    _LOGGER.info("STARTING")
+    executor.start_polling(dp)
--- utils.py
+++ utils.py
@@ -1,9 +1,15 @@
-HELP_MESSAGE = """Bot acos-exam gpt3.
-This bot uses gpt-3.5-turbo engine for generating responses for your messages.
-Send your request as usual message. Bot will send a response soon. Use commands:
-/help - for call this help message;
-/profile - to see information about your profile;
-/edit_profile - to edit profile: change parse mode;
-/clear - to clear conversation history.
+import openai
+
+HELP_MESSAGE = """Bot with gpt-3.5-turbo engine.
+Команды:
+/clear - очистить историю сообщений
+/help - вывод справки
+Просто напишите текст запроса, чтобы получить ответ от gpt-модели
 Bot made by A.B. group. avbudkova@gmail.com
 """
+
+
+def get_response(client: openai.OpenAI, history: list[tuple[str, str]]):
+    cooked_history = [{'role': 'system' if record[1] else 'user', 'content': record[0]} for record in history]
+    response = client.chat.completions.create(messages=cooked_history, model='gpt-3.5-turbo')
+    return response.choices[0].message.content
